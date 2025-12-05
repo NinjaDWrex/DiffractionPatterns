5c639526f9952633b8908957d25e5042a56446b7
Kerem Yaprak <kerem.yaprak555@gmail.com>
2025-12-06 00:03:22 +1300

    finished the programming of the photon diffraction logic. Now need to do drawing logic for all of the componenets

--- src/usr_gen_types/Photon.hpp
+++ src/usr_gen_types/Photon.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "../wave_angle_library/src/AngleGen.hpp"
 #include "PhotonSource.hpp"
 #include "raylib.h"
 #include "raymath.h"
@@ -10,6 +11,7 @@ class Photon {
     Vector2 Velocity;
     int Wavelength;
     Color color;
+    bool passed;
     std::vector<Slit> slits;
     Vector2 Vector2Scale(Vector2 vec, int scale){
       Vector2 buf;
@@ -28,25 +30,41 @@ class Photon {
       Wavelength = wavelength;
       color = c;
       ParentSource = p;
+      passed = false;
     }
     void Update(float dt){
       Vector2 dd = Vector2Scale(Velocity, dt);
       Pos = Vector2Add(Pos, dd);
       DistanceTravelled += Vector2Length(dd);
     }
+
     //draw logic
     void Draw() const {
       //lets draw the photons as smaller circles. Give them a colour and draw their "tail" 
     }
+
+    Slit ClosestSlit(){
+      float minDist = 999999999f;
+      Slit smallest;
+      for (auto s : slits){
+        float midPointX = (s.slitRep.p1.x + s.slitRep.p2.x) / 2;
+        float midPointY = (s.slitRep.p1.y + s.slitRep.p2.y) / 2;
+        float dist = Vector2Distance(Pos, {midPointX, midPointY});
+        if (minDist > dist) {
+            minDist = dist;
+            smallest = s;
+           }
+      }
+      return smallest;
+    }
+
     void UpdateVelocity(){
-      // we can change the value of v using
-      /**v =*/
-      /* to work out the change to velocity,
-       * we need to find when the light ray has passed through the diffraction barrier (the slit)
-       */
-      /* we want to be able to make a random selection of positions that the wave can "split" into.
-       * to do this, we can use a probability distribution linked to angles from the original path.
-       * then based off of this, we can make a new ray with this angle.]
-       */
+      //check if it has passed the closest slit
+      Vector2 closestSlit = ClosestSlit();
+      if (closestSlit.CheckIfPassed(this) && !passed){
+        float angle = generate_angle();
+        Velocity = Vector2Rotate(Velocity, angle);
+        passed = true;
+       }
     }
 };
--- src/wave_angle_library/src/lib.rs
+++ src/wave_angle_library/src/lib.rs
@@ -1,4 +1,11 @@
+use rand::thread_rng;
+use rand_distr::{Distribution, Normal};
+use std::os::raw::c_float;
 #[unsafe(no_mangle)]
-pub extern "C" fn GenerateAngle() -> i32 {
-    println!("Hello, world!");
+pub extern "C" fn generate_angle() -> c_float {
+    let mean: c_float = 0.0;
+    let sd: c_float = 0.785;
+    let dist = Normal::new(mean, sd).unwrap();
+    let mut rng = thread_rng();
+    dist.sample(&mut rng)
 }
